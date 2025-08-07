ac87870cc3ba172042ec01f5ad5b759cfce5b395
Zhenyan.Wang <zhenyan.wang@item.local>
2025-08-07 09:35:58 +0800

    fix: edit question

--- packages/flowFlex-common/src/app/views/onboard/checkList/components/WorkflowAssignments.vue
+++ packages/flowFlex-common/src/app/views/onboard/checkList/components/WorkflowAssignments.vue
@@ -48,6 +48,7 @@
 									:key="workflow.id"
 									:label="workflow.name"
 									:value="workflow.id"
+									:disabled="workflow.status === 'inactive'"
 								>
 									<div class="flex items-center justify-between">
 										<span>{{ workflow.name }}</span>
