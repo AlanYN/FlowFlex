2a94ce18b97ebeba7eaa2048c9712be2d166ff9f
Zhenyan.Wang <zhenyan.wang@item.local>
2025-10-20 14:46:55 +0800

    feat: add team menu

---
+++ packages/flowFlex-common/src/app/router/routers/modules/teams.ts
@@ -0,0 +1,37 @@
+import type { AppRouteModule } from '@/router/types';
+import { LAYOUT } from '@/router/constant';
+import { t } from '@/hooks/useI18n';
+
+import teamsIcon from '@assets/svg/menu/teams.svg';
+
+const teams: AppRouteModule = {
+	path: '/teams',
+	name: 'Teams',
+	component: LAYOUT,
+	redirect: '/teams/index',
+	meta: {
+		hideChildrenInMenu: true,
+		icon: teamsIcon,
+		title: t('sys.router.teamsManagement'),
+		ordinal: 10,
+		code: 'TEAMS',
+		hidden: false,
+		status: true,
+	},
+	children: [
+		{
+			path: 'index',
+			name: 'TeamsManagement',
+			component: () => import('@/views/authorityManagement/teams.vue'),
+			meta: {
+				title: t('sys.router.teamsManagement'),
+				ordinal: 1,
+				code: 'TEAMS',
+				hidden: false,
+				status: true,
+			},
+		},
+	],
+};
+
+export default teams;
---
+++ packages/flowFlex-common/src/app/views/authorityManagement/teams.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="w-full h-full wfe-global-block-bg p-4" ref="scrollbarRef">
+		<iframe
+			ref="iframeRef"
+			:src="iframeUrl"
+			frameborder="0"
+			id="permission-iframe"
+			class="w-full h-full border-none"
+		></iframe>
+	</div>
+</template>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import { useUserStoreWithOut } from '@/stores/modules/user';
+import { getTokenobj } from '@/utils/auth';
+import type { TokenObj } from '@/apis/axios/Axios';
+import { useGlobSetting } from '@/settings/';
+import { ProjectEnum } from '@/enums/appEnum';
+import { useIframeMessage } from '@/hooks/useIframeMessage';
+import { useAdaptiveScrollbar } from '@/hooks/useAdaptiveScrollbar';
+
+const { scrollbarRef } = useAdaptiveScrollbar();
+
+const userStore = useUserStoreWithOut();
+const settings = useGlobSetting();
+
+// const BASE_DOMAIN = settings.ssoURL;
+// const BASE_URL = `${BASE_DOMAIN}permission/user`;
+const APP_ID = ProjectEnum.WFE;
+const iframeUrl = ref();
+const iframeRef = ref();
+
+// Use the iframe message hook
+useIframeMessage();
+
+onMounted(() => {
+	const tokenObj = getTokenobj() as TokenObj;
+	const userInfo = userStore.getUserInfo || {};
+	const token = tokenObj?.accessToken?.token;
+	const { userId } = userInfo;
+	const theme = localStorage.theme;
+	const primary = localStorage.primary || 'blue';
+	iframeUrl.value = `${settings.idmUrl}/permission/teams?appId=${APP_ID}&userId=${userId}&appToken=${token}&theme=${theme}&primary=${primary}`;
+});
+</script>
