d551574c218e7f4b4c086517b6cb02edef78c617
dengwang <deng.wang@item.com>
2025-11-05 18:31:04 +0800

    fix: export table

--- packages/flowFlex-backend/Application/Services/OW/OnboardingService.cs
+++ packages/flowFlex-backend/Application/Services/OW/OnboardingService.cs
@@ -3365,7 +3365,7 @@ namespace FlowFlex.Application.Services.OW
             // Set headers
             var headers = new[]
             {
-                "Customer Name", "Lead ID", "Case Code", "Contact Name", "Life Cycle Stage", "Workflow", "Stage",
+                "Customer Name", "Case Code", "Lead ID", "Contact Name", "Life Cycle Stage", "Workflow", "Stage",
                 "Priority", "Ownership", "Status", "Start Date", "End Date", "Updated By", "Update Time"
             };
 
@@ -3380,8 +3380,8 @@ namespace FlowFlex.Application.Services.OW
             {
                 var item = data[row];
                 worksheet.Cells[row + 2, 1].Value = item.CustomerName;
-                worksheet.Cells[row + 2, 2].Value = item.Id;
-                worksheet.Cells[row + 2, 3].Value = item.CaseCode;
+                worksheet.Cells[row + 2, 2].Value = item.CaseCode;
+                worksheet.Cells[row + 2, 3].Value = item.Id;
                 worksheet.Cells[row + 2, 4].Value = item.ContactName;
                 worksheet.Cells[row + 2, 5].Value = item.LifeCycleStage;
                 worksheet.Cells[row + 2, 6].Value = item.WorkFlow;
